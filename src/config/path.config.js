b2c9cb2749ad4a038d8115b1d18e4a67a334a7c1
Joseph Itakala <josephitakala18@gmail.com>
2025-03-05 09:56:23 +0100

    feat: Creates basic config like jwt-passport strategies and path configs

---
+++ src/config/path.config.js
@@ -0,0 +1,16 @@
+const authBaseURI = "/api/auth"; /** Authentification **/
+
+const studentsBaseURI = "/api/students"; /** Gestion des élèves **/
+
+const agentsBaseURI = "/api/agents"; /** Gestion des agents **/
+
+const parentsBaseURI = "/api/parents"; /** Gestion des parents **/
+
+const paths = {
+  authBaseURI: authBaseURI,
+  studentsBaseURI: studentsBaseURI,
+  agentsBaseURI: agentsBaseURI,
+  parentsBaseURI: parentsBaseURI,
+};
+
+module.exports = paths;
