a21979168eaa5ee3ecd117fd4b5628acb626206d
Joseph Itakala <josephitakala18@gmail.com>
2025-04-27 23:26:39 +0200

    Trying fix bug on pagination.js - 2

--- src/utils/pagination.js
+++ src/utils/pagination.js
@@ -1,46 +1,53 @@
 async function paginationQuery(model, page = 1, limit = 10, options = {}) {
-  page = Math.max(1, parseInt(page) || 1);
-  limit = Math.max(1, parseInt(limit) || 10);
-  const skip = (page - 1) * limit;
-
-  if (!model || !model.count) {
-    // throw new Error("Modèle invalide fourni à paginationQuery");
-    return {
-      error: "Modèle invalide fourni à paginationQuery"
+  try {
+    page = Math.max(1, parseInt(page) || 1);
+    limit = Math.max(1, parseInt(limit) || 10);
+    const skip = (page - 1) * limit;
+  
+    if (!model || !model.count) {
+      return {
+        error: `Modèle invalide fourni à paginationQuery. Modele: ${model}`
+      }
     }
-  }
-
-  // Récuperation des filtres s'ils existent
-  const where = options.where || {};
-
-  //Récupération du nombre total d'éléments
-  const totalItems = await model.count({ where });
-  const totalPages = Math.ceil(totalItems / limit);
-
-  // Vérifier si la page demandée est valide
-  if (page > totalPages && totalItems > 0) {
+  
+    // Récuperation des filtres s'ils existent
+    const where = options.where || {};
+  
+    //Récupération du nombre total d'éléments
+    const totalItems = await model.count({ where });
+    const totalPages = Math.ceil(totalItems / limit);
+  
+    // Vérifier si la page demandée est valide
+    if (page > totalPages && totalItems > 0) {
+      return {
+        error: `La page ${page} n'existe pas. Dernière page disponible : ${totalPages}.`,
+        totalItems,
+        limitPerPage: limit,
+        totalPages,
+        currentPage: page,
+        data: [],
+      };
+    }
+  
+    //Récupération des éléments paginés
+    const queryOptions = { skip, take: limit, ...options };
+  
+    const data = await model.findMany(queryOptions);
+  
     return {
-      error: `La page ${page} n'existe pas. Dernière page disponible : ${totalPages}.`,
       totalItems,
       limitPerPage: limit,
       totalPages,
       currentPage: page,
-      data: [],
+      data,
+    };
+  }catch (error) {
+    console.error("Erreur dans paginationQuery:", error);
+    return {
+      error: "Erreur lors de la pagination",
+      details: error.message
     };
   }
-
-  //Récupération des éléments paginés
-  const queryOptions = { skip, take: limit, ...options };
-
-  const data = await model.findMany(queryOptions);
-
-  return {
-    totalItems,
-    limitPerPage: limit,
-    totalPages,
-    currentPage: page,
-    data,
-  };
 }
 
 module.exports = paginationQuery;
